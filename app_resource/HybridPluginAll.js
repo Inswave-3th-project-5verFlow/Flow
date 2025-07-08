daec53d40c9b7c245a13138f9c087c76c960fcdc
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-07-08 19:54:05 +0900

    Delete app_resource directory

--- app_resource/HybridPluginAll.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	if(typeof top.wmatrix != "object"){
-		if (navigator.userAgent.indexOf('Android') > 0) {
-			wmatrixBridge.loadJS("wmatrix.js");
-		} else {
-			window.webkit.messageHandlers.wmatrix.postMessage(["WMatrix","loadJS:","wmatrix.js"]);
-		}
-	}
-})();
